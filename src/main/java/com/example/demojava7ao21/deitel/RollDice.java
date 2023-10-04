fa800ac5c9b67b904aa056e5d3670d7e7c3bccbb
ivsonsantos <ivson.java@gmail.com>
2023-10-04 21:58:45 +0200

    Deitel exemplos

---
+++ src/main/java/com/example/demojava7ao21/deitel/RollDice.java
@@ -0,0 +1,24 @@
+package com.example.demojava7ao21.deitel;
+
+import java.security.SecureRandom;
+
+public class RollDice {
+
+    public static void main(String[] args) {
+
+        SecureRandom numeros = new SecureRandom();
+
+        int[] frequency = new int[7];
+
+        for (int roll = 1; roll < 6000000; roll++) {
+            ++frequency[1 + numeros.nextInt(6)];
+        }
+
+        System.out.printf("%s%10s%n", "Face", "Frequency");
+
+        for (int face = 1; face < frequency.length; face++) {
+            System.out.printf("%4d%10d%n", face, frequency[face]);
+        }
+
+    }
+}
