c7b4ef54b06a5dd43c07df6388c351a61f3b58bc
ivsonsantos <ivson.java@gmail.com>
2023-10-04 12:10:19 +0200

    Projeto criado

---
+++ src/main/java/com/example/demojava7ao21/DemoJava7ao21Application.java
@@ -0,0 +1,13 @@
+package com.example.demojava7ao21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJava7ao21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJava7ao21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demojava7ao21/DemoJava7ao21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojava7ao21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJava7ao21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
