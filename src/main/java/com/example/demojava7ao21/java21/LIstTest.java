82e0c7e61bec31e04ced02b46a024f5f774dd880
ivsonsantos <ivson.java@gmail.com>
2023-10-05 21:46:24 +0200

    Java 21 Collections

---
+++ src/main/java/com/example/demojava7ao21/java21/LIstTest.java
@@ -0,0 +1,17 @@
+package com.example.demojava7ao21.java21;
+
+import java.util.ArrayList;
+
+public class LIstTest {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1); // [1]
+        arrayList.addFirst(0); // [0, 1]
+        arrayList.addLast(2); // [0, 1, 2]
+        arrayList.getFirst(); // 0
+        arrayList.getLast(); // 2
+        arrayList.reversed(); // [2, 1, 0]
+    }
+}
---
+++ src/main/java/com/example/demojava7ao21/java21/SequenceSetTest.java
@@ -0,0 +1,18 @@
+package com.example.demojava7ao21.java21;
+
+import java.util.LinkedHashSet;
+
+public class SequenceSetTest {
+
+    public static void main(String[] args) {
+
+        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add(1); // [1]
+        linkedHashSet.addFirst(0); // [0, 1]
+        linkedHashSet.addLast(2); // [0, 1, 2]
+        linkedHashSet.getFirst(); // 0
+        linkedHashSet.getLast(); // 2
+        System.out.println(linkedHashSet.reversed()); // [2, 1, 0]
+    }
+
+}
