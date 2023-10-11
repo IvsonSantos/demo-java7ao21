fa557b1a0f03c1774b2bd0358bdb9b626182b416
ivsonsantos <ivson.java@gmail.com>
2023-10-11 20:05:59 +0200

    Java 21 Record

---
+++ src/main/java/com/example/demojava7ao21/java21/string/RecordTest.java
@@ -0,0 +1,15 @@
+package com.example.demojava7ao21.java21.string;
+
+public class RecordTest {
+
+    // This concise syntax results in a class with a constructor, associated storage in the course,
+    // getters (e.g.: event.name()), a valid equals, and a good toString() implementation.
+    record RecordEvent(String name) { }
+
+    public static void main(String[] args) {
+        var event = new RecordEvent("Java21");
+
+        System.out.println(event.name());
+        System.out.println(event);
+    }
+}
