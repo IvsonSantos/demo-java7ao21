6247b0bdec9d587c52c5b75182f7b9606334a7e9
ivsonsantos <ivson.java@gmail.com>
2023-10-11 20:14:20 +0200

    Java 21 Switch

---
+++ src/main/java/com/example/demojava7ao21/java21/newswitch/SwitchTest.java
@@ -0,0 +1,33 @@
+package com.example.demojava7ao21.java21.newswitch;
+
+import java.time.DayOfWeek;
+
+public class SwitchTest {
+
+    // old switch
+    static int calculateTimeOffClassic(DayOfWeek dayOfWeek) {
+        var timeoff = 0;
+        switch (dayOfWeek) {
+            case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY:
+                timeoff = 16;
+                break;
+            case SATURDAY, SUNDAY:
+                timeoff = 24;
+                break;
+        }
+        return timeoff;
+    }
+
+    // new switch
+    static int calculateTimeOff(DayOfWeek dayOfWeek) {
+        return switch (dayOfWeek) {
+            case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> 16;
+            case SATURDAY, SUNDAY -> 24;
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateTimeOffClassic(DayOfWeek.SATURDAY));
+        System.out.println(calculateTimeOff (DayOfWeek.SATURDAY));
+    }
+}
