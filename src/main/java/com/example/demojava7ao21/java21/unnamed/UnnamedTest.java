46f57f30e8fedbb70ef392bc1621dc758ebacb33
ivsonsantos <ivson.java@gmail.com>
2023-10-12 18:33:29 +0200

    Java 21

---
+++ src/main/java/com/example/demojava7ao21/java21/unnamed/UnnamedTest.java
@@ -0,0 +1,22 @@
+package com.example.demojava7ao21.java21.unnamed;
+
+public class UnnamedTest {
+
+    public static void main(String[] args) {
+        try {
+            //...
+        } catch (Exception _) {
+            // we don't need the actual exception
+        }
+    }
+
+    int acc = 0;
+    /*
+    for(Order _: orders){
+        if (acc < 10) {
+            // the actual order is not used
+        }
+    }
+     */
+
+}
