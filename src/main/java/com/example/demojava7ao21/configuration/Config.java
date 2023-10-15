c995b99cbbb3ecf16282e46647e8a63e8f55dd88
ivsonsantos <ivson.java@gmail.com>
2023-10-15 19:11:24 +0200

    Spring singleton

--- src/main/java/com/example/demojava7ao21/configuration/Config.java
+++ src/main/java/com/example/demojava7ao21/configuration/Config.java
@@ -1,8 +1,11 @@
 package com.example.demojava7ao21.configuration;
 
+import com.example.demojava7ao21.singleton.Person;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan("com.example.demojava7ao21")
@@ -16,6 +19,13 @@ public class Config {
     @Bean
     public Transmission transmission() {
         return new Transmission("sliding");
+    }
+
+    @Bean
+    //@Scope("singleton")
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public Person personSingleton() {
+        return new Person();
     }
 
 }
---
+++ src/main/java/com/example/demojava7ao21/singleton/Person.java
@@ -0,0 +1,9 @@
+package com.example.demojava7ao21.singleton;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private String name;
+}
