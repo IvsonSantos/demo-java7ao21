a8e911ab0eaf7eec31c9dfb6027e9704551d64fb
ivsonsantos <ivson.java@gmail.com>
2023-10-29 19:58:08 +0100

    inserir dados no H2 do spring 2.4

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into person (nome) values ('ivson');
+insert into person (nome) values ('ivson');
+insert into person (nome) values ('ivson');
+insert into person (nome) values ('algebra');
+insert into person (nome) values ('teste');
+insert into person (nome) values ('matematica');
+insert into person (nome) values ('xis');
